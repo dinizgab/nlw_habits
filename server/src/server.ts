75e60c41292846f19a5430c58939caed201a5a99
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2023-01-18 22:51:46 -0300

    Starting backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,11 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
-import { PrismaClient } from "@prisma/client";
+import { appRoutes } from "./routes";
 
 const app = Fastify();
-const prisma = new PrismaClient();
 
 app.register(cors);
-
-app.get("/hello", () => {});
+app.register(appRoutes)
 
 app
   .listen({
