2e06d98c1b26841a3d861a87e9cf6cce70414524
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2023-01-22 14:37:53 -0300

    Connecting frontend and backend

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,16 +1,45 @@
+import * as Checkbox from "@radix-ui/react-checkbox";
 import * as PopOver from "@radix-ui/react-popover";
 import clsx from "clsx";
 import ProgressBar from "./ProgressBar";
+import { Check } from "phosphor-react";
+import dayjs from "dayjs";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 interface HabitDayProps {
-  amount: number;
-  amountCompleted: number;
+  date: Date;
+  amount?: number;
+  amountCompleted?: number;
 }
 
-export default function HabitDay(props: HabitDayProps) {
-  const percentageCompleted = Math.ceil(
-    (props.amountCompleted / props.amount) * 100
-  );
+export default function HabitDay({
+  amountCompleted = 0,
+  amount = 0,
+  date,
+}: HabitDayProps) {
+  const [thisDayHabits, setThisDayHabits] = useState<string[]>([]);
+
+  const percentageCompleted =
+    amount > 0 ? Math.ceil((amountCompleted / amount) * 100) : 0;
+
+  const dayAndMonth = dayjs(date).format("DD/MM");
+  const dayOfWeek = dayjs(date).format("dddd");
+
+  /* useEffect(() => {
+    const availableHabits: string[] = [];
+    const formatedQueryDate = dayjs(date).format("YYYY-MM-DDTHH:mm:ss.SSS[Z]")
+
+
+
+    axios.get(`/day?date=${formatedQueryDate}`).then((res) => {
+      res.data.map((habit: { title: string }) =>
+        availableHabits.push(habit.title)
+      );
+    });
+
+    setThisDayHabits(availableHabits);
+  }, []); */
 
   return (
     <PopOver.Root>
@@ -34,12 +63,29 @@ export default function HabitDay(props: HabitDayProps) {
 
       <PopOver.Portal>
         <PopOver.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col text-white">
-          <span className="font-semibold text-zinc-400 ">Sabado</span>
+          <span className="font-semibold text-zinc-400 ">{dayOfWeek}</span>
           <span className="mt-1 font-extrabold text-3xl leading-tight">
-            21/01
+            {dayAndMonth}
           </span>
 
           <ProgressBar progress={percentageCompleted} />
+
+          <div className="mt-6 flex flex-col gap-3">
+            <Checkbox.Root className="flex items-center gap-3 group">
+              <div className="h-8 w-8 flex items-center justify-center bg-zinc-900 border-2 rounded-lg border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+                <Checkbox.Indicator>
+                  <Check size={20} className="text-white" />
+                </Checkbox.Indicator>
+              </div>
+
+              {thisDayHabits.map((habit) => (
+                <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-300">
+                  {habit}
+                </span>
+              ))}
+            </Checkbox.Root>
+          </div>
+
           <PopOver.Arrow height={8} width={16} className="fill-zinc-900" />
         </PopOver.Content>
       </PopOver.Portal>
--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,8 +1,52 @@
+import * as Checkbox from "@radix-ui/react-checkbox";
 import { Check } from "phosphor-react";
+import { FormEvent, useState } from "react";
+import { api } from "../lib/axios";
+
+const availableWeekDays = [
+  "Domingo",
+  "Segunda-feira",
+  "Terca-feira",
+  "Quarta-feira",
+  "Quinta-feira",
+  "Sexta-feira",
+  "Sabado",
+];
 
 export default function NewHabitForm() {
+  const [title, setTitle] = useState<string>("");
+  const [weekDays, setWeekDays] = useState<number[]>([]);
+
+  async function createNewHabit(event: FormEvent) {
+    event.preventDefault();
+    if (!title || weekDays.length === 0) {
+      return;
+    }
+
+    await api.post("/habits", {
+      title,
+      weekDays,
+    });
+
+    setTitle("")
+    setWeekDays([])
+  }
+
+  function handleDayCheck(weekDay: number) {
+    if (weekDays.includes(weekDay)) {
+      const weekDaysWithRemoved = weekDays.filter((day) => day !== weekDay);
+      setWeekDays(weekDaysWithRemoved);
+    } else {
+      const weekDaysWithAdded = [...weekDays, weekDay];
+      setWeekDays(weekDaysWithAdded);
+    }
+  }
+
   return (
-    <form action="" className="w-full flex flex-col mt-6 text-white">
+    <form
+      onSubmit={createNewHabit}
+      className="w-full flex flex-col mt-6 text-white"
+    >
       <label htmlFor="title" className="font-semibold leading-tight">
         Qual seu comprometimento
       </label>
@@ -11,12 +55,34 @@ export default function NewHabitForm() {
         id="title"
         placeholder="ex.: Exercicios, dormir bem, etc..."
         autoFocus
-        className="p-4 rounded-lg mt-3 bg-zinc-800 placeholder:text-zinc-400"
+        className="p-4 rounded-lg mt-3 bg-zinc-800 placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-zinc-900"
+        onChange={(event) => setTitle(event.target.value)}
+        value={title}
       />
 
       <label htmlFor="" className="font-semibold leading-tight mt-4">
         Qual a recorrencia?
       </label>
+
+      <div className="flex flex-col gap-2 mt-3">
+        {availableWeekDays.map((day, index) => (
+          <Checkbox.Root
+            key={day}
+            className="flex items-center gap-3 group"
+            onCheckedChange={() => {
+              handleDayCheck(index);
+            }}
+            checked={weekDays.includes(index)}
+          >
+            <div className="h-8 w-8 flex items-center justify-center bg-zinc-900 border-2 rounded-lg border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+              <Checkbox.Indicator>
+                <Check size={20} className="text-white" />
+              </Checkbox.Indicator>
+            </div>
+            <span className="text-white leading-tigh">{day}</span>
+          </Checkbox.Root>
+        ))}
+      </div>
 
       <button
         type="submit"
